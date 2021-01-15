5570774823e6e3060176a71d7e8323e42946509a
TungAn5055 <tungan5055@gmail.com>
2021-01-15 23:05:19 +0700

    add code

--- config/requestAxios.js
+++ config/requestAxios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://vietship.de';
+axios.defaults.baseURL = 'https://vietship.de/';
 axios.defaults.headers.common.Authorization =
   'Bearer 4kmdxwstu5562ykoz96nwa8ehroau1ms';
