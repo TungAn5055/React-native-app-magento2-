bf91664ea0fcc561ddc2f119a9009fbf055804f1
NamDD <namdd@smartosc.com>
2021-01-16 17:23:46 +0700

    add code fix sass

--- src/common/index.js
+++ src/common/index.js
@@ -3,7 +3,7 @@ import Card from './Card/Card';
 import Divider from './Divider/Divider';
 import Icon from './Icon/Icon';
 // import Image from './Image/Image';
-// import LoadingView from './LoadingView/LoadingView';
+import LoadingView from './LoadingView/LoadingView';
 // import MessageView from './MessageView/MessageView';
 import Price from './Price/Price';
 import Text from './Text/Text';
@@ -14,7 +14,7 @@ export {
   Divider,
   Icon,
   // Image,
-  // LoadingView,
+  LoadingView,
   // MessageView,
   Price,
   Text,
